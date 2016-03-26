5a1acca80c3b22834bd76aa89f5aff05ccceff8f
stefanocassio <stefano.cassiobkk@gmail.com>
2016-03-27 00:12:02 +0700

    servers failing at postgress invalid password

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 set :application, 'trial_site'
 set :repo_url, 'git@github.com:stefanobkk/Trackstor.git'
-set :rvm_ruby_version, '2.2.1@trial_site'
+set :rvm_ruby_version, '2.2.1'
 set :deploy_to, '/home/stefanobkk/trial_site'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
